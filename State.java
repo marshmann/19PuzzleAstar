8fc0bf9380e844ded38021052fb8bfc08ca3a480
marshmann <nmarshman96@gmail.com>
2018-07-13 14:36:42 -0400

    Fixed spacing (tabs -> 4 spaces)

--- State.java
+++ State.java
@@ -1,23 +1,23 @@
 public class State {
-	int[][] board;
-	public State(int board[][]) {
-		this.board = board;
-	}
-	public boolean equals(Object obj) {
-		for(int i = 0; i < 6; i++) {
-			for(int j = 0; j<6;j++) {
-				if(((State)obj).board[i][j] != this.board[i][j]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	public int hashCode() {
-		int hash = 0;
-	    for (int i = 0; i < 6; i++)
-	        for (int j = 0; j < 6; j++)
-	            hash = hash * 31 + board[i][j];
-	    return hash;
-	}
+    int[][] board;
+    public State(int board[][]) {
+        this.board = board;
+    }
+    public boolean equals(Object obj) {
+        for(int i = 0; i < 6; i++) {
+            for(int j = 0; j<6;j++) {
+                if(((State)obj).board[i][j] != this.board[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public int hashCode() {
+        int hash = 0;
+        for (int i = 0; i < 6; i++)
+            for (int j = 0; j < 6; j++)
+                hash = hash * 31 + board[i][j];
+        return hash;
+    }
 }
